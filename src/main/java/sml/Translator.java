dab2b63a55fbe508d78b6fdd862eb38cb34601b8
RyanDur <ryan.p.durling@gmail.com>
2014-11-23 00:46:12 +0000

    refactor tanslator

--- src/main/java/sml/Translator.java
+++ src/main/java/sml/Translator.java
@@ -1,133 +1,115 @@
 package sml;
 
-import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /*
  * The translator of a <b>S</b><b>M</b>al<b>L</b> program.
  */
 public class Translator {
 
-	// word + line is the part of the current line that's not yet processed
-	// word has no whitespace
-	// If word and line are not empty, line begins with whitespace
-	private String line = "";
-	private Labels labels; // The labels of the program being translated
-	private ArrayList<Instruction> program; // The program to be created
-	private String fileName; // source file of SML code
+    // word + line is the part of the current line that's not yet processed
+    // word has no whitespace
+    // If word and line are not empty, line begins with whitespace
+    private String line = "";
+    private Labels labels; // The labels of the program being translated
+    private ArrayList<Instruction> program; // The program to be created
+    private String fileName; // source file of SML code
 
-	private static final String SRC = "src";
+    private static final String SRC = "src";
 
-	public Translator(String fileName) {
-		this.fileName = SRC + "/" + fileName;
-	}
+    public Translator(String fileName) {
+        this.fileName = fileName;
+    }
 
-	// translate the small program in the file into lab (the labels) and
-	// prog (the program)
-	// return "no errors were detected"
-	public boolean readAndTranslate(Labels lab, ArrayList<Instruction> prog) {
-		Scanner sc; // Scanner attached to the file chosen by the user
-		try {
-			sc = new Scanner(new File(fileName));
-		} catch (IOException ioE) {
-			System.out.println("File: IO error to start " + ioE.getMessage());
-			return false;
-		}
-		labels = lab;
-		labels.reset();
-		program = prog;
-		program.clear();
+    // translate the small program in the file into lab (the labels) and
+    // prog (the program)
+    // return "no errors were detected"
+    public boolean readAndTranslate(Labels lab, ArrayList<Instruction> prog) {
+        labels = lab;
+        labels.reset();
+        program = prog;
+        program.clear();
+        try {
+            // Each iteration processes line and reads the next line into line
+            Files.lines(Paths.get(SRC, fileName)).forEach(line -> {
+                this.line = line;
+                String label = scan();
+                if (label.length() > 0) {
+                    Instruction ins = getInstruction(label);
+                    if (ins != null) {
+                        labels.addLabel(label);
+                        program.add(ins);
+                    }
+                }
+            });
+        } catch (IOException e) {
+            System.out.println("File: IO error to start " + e.getMessage());
+            return false;
+        }
+        return true;
+    }
 
-		try {
-			line = sc.nextLine();
-		} catch (NoSuchElementException ioE) {
-			return false;
-		}
+    // line should consist of an MML instruction, with its label already
+    // removed. Translate line into an instruction with label label
+    // and return the instruction
+    public Instruction getInstruction(String label) {
+        int s1; // Possible operands of the instruction
+        int s2;
+        int r;
+        int x;
 
-		// Each iteration processes line and reads the next line into line
-		while (line != null) {
-			// Store the label in label
-			String label = scan();
+        if (line.equals(""))
+            return null;
 
-			if (label.length() > 0) {
-				Instruction ins = getInstruction(label);
-				if (ins != null) {
-					labels.addLabel(label);
-					program.add(ins);
-				}
-			}
+        String ins = scan();
+        switch (ins) {
+            case "add":
+                r = scanInt();
+                s1 = scanInt();
+                s2 = scanInt();
+                return new AddInstruction(label, r, s1, s2);
+            case "lin":
+                r = scanInt();
+                s1 = scanInt();
+                return new LinInstruction(label, r, s1);
+        }
 
-			try {
-				line = sc.nextLine();
-			} catch (NoSuchElementException ioE) {
-				return false;
-			}
-		}
-		return true;
-	}
+        // You will have to write code here for the other instructions.
 
-	// line should consist of an MML instruction, with its label already
-	// removed. Translate line into an instruction with label label
-	// and return the instruction
-	public Instruction getInstruction(String label) {
-		int s1; // Possible operands of the instruction
-		int s2;
-		int r;
-		int x;
+        return null;
+    }
 
-		if (line.equals(""))
-			return null;
+    /*
+     * Return the first word of line and remove it from line. If there is no
+     * word, return ""
+     */
+    public String scan() {
+        line = line.trim();
+        if (line.length() == 0) return "";
+        List<String> words = Arrays.stream(line.split(" ")).collect(Collectors.toList());
+        String word = words.remove(0);
+        line = words.stream().collect(Collectors.joining(" "));
+        return word;
+    }
 
-		String ins = scan();
-		switch (ins) {
-		case "add":
-			r = scanInt();
-			s1 = scanInt();
-			s2 = scanInt();
-			return new AddInstruction(label, r, s1, s2);
-		case "lin":
-			r = scanInt();
-			s1 = scanInt();
-			return new LinInstruction(label, r, s1);
-		}
+    // Return the first word of line as an integer. If there is
+    // any error, return the maximum int
+    public int scanInt() {
+        String word = scan();
+        if (word.length() == 0) {
+            return Integer.MAX_VALUE;
+        }
 
-		// You will have to write code here for the other instructions.
-
-		return null;
-	}
-
-	/*
-	 * Return the first word of line and remove it from line. If there is no
-	 * word, return ""
-	 */
-	public String scan() {
-		line = line.trim();
-		if (line.length() == 0) return "";
-
-		int i = 0;
-		while (i < line.length() && line.charAt(i) != ' ' && line.charAt(i) != '\t') {
-			i = i + 1;
-		}
-		String word = line.substring(0, i);
-		line = line.substring(i);
-		return word;
-	}
-
-	// Return the first word of line as an integer. If there is
-	// any error, return the maximum int
-	public int scanInt() {
-		String word = scan();
-		if (word.length() == 0) {
-			return Integer.MAX_VALUE;
-		}
-
-		try {
-			return Integer.parseInt(word);
-		} catch (NumberFormatException e) {
-			return Integer.MAX_VALUE;
-		}
-	}
+        try {
+            return Integer.parseInt(word);
+        } catch (NumberFormatException e) {
+            return Integer.MAX_VALUE;
+        }
+    }
 }
--- src/test/java/sml/TranslatorTest.java
+++ src/test/java/sml/TranslatorTest.java
@@ -1,19 +1,35 @@
 package sml;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
 
 public class TranslatorTest {
 
+    private Translator translator;
+    private Labels labels;
+    private ArrayList<Instruction> list;
+
+    @Before
+    public void setup() {
+        translator = new Translator("instructions.txt");
+        labels = mock(Labels.class);
+        list = new ArrayList<>();
+    }
+
     @Test
     public void shouldBeAbleToReadAndTranslate() {
-        Translator translator = new Translator("instructions.txt");
-        Labels labels = new Labels();
-        ArrayList<Instruction> list = new ArrayList<>();
         assertThat(translator.readAndTranslate(labels, list), is(true));
+    }
+
+    @Test
+    public void shouldBeAbleToGetTheFirstWordFromALine() {
+        String line = "Add something";
+        System.out.println(translator.scan());
     }
 }
